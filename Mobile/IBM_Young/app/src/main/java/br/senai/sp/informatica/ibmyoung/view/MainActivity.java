55d48b241b70539a94de8fd38870a4be81eb30c8
JSWilProf <penawjs@hotmail.com>
2018-03-26 21:14:36 -0300

    Carga da Transação Principal em 26/03/2018 21:14

--- Mobile/IBM_Young/app/src/main/java/br/senai/sp/informatica/ibmyoung/view/MainActivity.java
+++ Mobile/IBM_Young/app/src/main/java/br/senai/sp/informatica/ibmyoung/view/MainActivity.java
@@ -1,17 +1,64 @@
 package br.senai.sp.informatica.ibmyoung.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import br.senai.sp.informatica.ibmyoung.R;
 
 
-// TODO: efetuar uma consulta para determinar qual nivel está o usuário
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView btRanking;
+    private ImageView btPerfil;
+    private ImageView btNivel1;
+    private ImageView btNivel2;
+    private ImageView btNivel3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        btRanking = findViewById(R.id.btRanking);
+        btRanking.setOnClickListener(this);
+
+        btPerfil = findViewById(R.id.btPerfil);
+        btPerfil.setOnClickListener(this);
+
+        btNivel1 = findViewById(R.id.btNivel1);
+        btNivel1.setOnClickListener(this);
+
+        btNivel2 = findViewById(R.id.btNivel2);
+        btNivel2.setOnClickListener(this);
+
+        btNivel3 = findViewById(R.id.btNivel3);
+        btNivel3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.btRanking:
+                // chamar a tela de Classificação
+                startActivity(new Intent(this, PerfilActivity.class));
+                break;
+            case R.id.btPerfil:
+                // Chamar a tela de Perfil
+                startActivity(new Intent(this, PerfilActivity.class));
+                break;
+            case R.id.btNivel1:
+                // Consultar as tarefas no nivel 1
+                break;
+            case R.id.btNivel2:
+                // Consultar as tarefas no nivel 1
+                break;
+            case R.id.btNivel3:
+                // Consultar as tarefas no nivel 1
+                break;
+        }
     }
 }
